b80bd8c6473a79ceb926af8cc7813486b3751e5b
DevJoaoVitorB <jv23072006@gmail.com>
2026-03-03 10:56:12 -0300

    chore: configurar ambiente Vite + React + Styled Components

---
+++ calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from './global/GlobalStyles';
+
+function App() {
+    return (
+        <div>
+            <GlobalStyle />
+            <p>Hello World!</p>
+        </div>
+    );
+}
+
+export default App;
